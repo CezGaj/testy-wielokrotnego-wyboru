76e8d40a197162fb96efd1cb7a87429e7a7ad23f
CezGaj <68070366+CezGaj@users.noreply.github.com>
2020-07-13 20:40:13 +0200

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,6 +1,5 @@
 <?php
-$servername = "
-postgres://atortuulxmovtg:85f9b2b3be752b8c59218a1dd28c19f026488750c4a77d18054a95627f66810b@ec2-46-137-124-19.eu-west-1.compute.amazonaws.com:5432/d79rdvlg8o4982";
+$servername = parse_url(getenv('DATABASE_URL'));
 $username = "atortuulxmovtg";
 $password = "85f9b2b3be752b8c59218a1dd28c19f026488750c4a77d18054a95627f66810b";
 $dbname = "d79rdvlg8o4982";
