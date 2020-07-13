53cf2f0192b545eb8696e95b571e5228b44804a7
CezGaj <68070366+CezGaj@users.noreply.github.com>
2020-07-13 20:33:47 +0200

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,9 +1,10 @@
 <?php
-$servername = "ec2-46-137-124-19.eu-west-1.compute.amazonaws.com";
+$servername = "
+postgres://atortuulxmovtg:85f9b2b3be752b8c59218a1dd28c19f026488750c4a77d18054a95627f66810b@ec2-46-137-124-19.eu-west-1.compute.amazonaws.com:5432/d79rdvlg8o4982";
 $username = "atortuulxmovtg";
 $password = "85f9b2b3be752b8c59218a1dd28c19f026488750c4a77d18054a95627f66810b";
 $dbname = "d79rdvlg8o4982";
-$conn = new mysqli($servername, $username, $password, $dbname, '5432');
+$conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) 
 {
     die("Connection failed: " . $conn->connect_error);
