db125e23b416c8ecd606d53015dee093143edad3
CezGaj <68070366+CezGaj@users.noreply.github.com>
2020-07-13 19:55:03 +0200

    Update wynik.php

--- wynik.php
+++ wynik.php
@@ -89,9 +89,9 @@ $wynik=$wynik+$liczba;
 //echo "$wynik<br>";
 }
 $servername = "localhost";
-$username = "id5804479_admin";
-$password = "abcde";
-$dbname = "id5804479_db";
+$username = "atortuulxmovtg";
+$password = "85f9b2b3be752b8c59218a1dd28c19f026488750c4a77d18054a95627f66810b";
+$dbname = "d79rdvlg8o4982";
 $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) 
 {
