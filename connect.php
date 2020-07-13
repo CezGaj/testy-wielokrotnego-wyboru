12185cc79e535ee0b1122d6208c949d87b370090
CezGaj <68070366+CezGaj@users.noreply.github.com>
2020-07-13 20:03:00 +0200

    Create connect.php

---
+++ connect.php
@@ -0,0 +1,11 @@
+<?php
+$servername = "postgresql-rugged-83706";
+$username = "atortuulxmovtg";
+$password = "85f9b2b3be752b8c59218a1dd28c19f026488750c4a77d18054a95627f66810b";
+$dbname = "d79rdvlg8o4982";
+$conn = new mysqli($servername, $username, $password, $dbname);
+if ($conn->connect_error) 
+{
+    die("Connection failed: " . $conn->connect_error);
+}
+?>
