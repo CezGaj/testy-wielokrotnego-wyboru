d89e374dcd7d744a6abea3c5dcd9c9747d253bb4
CezGaj <68070366+CezGaj@users.noreply.github.com>
2020-07-13 20:03:46 +0200

    Update edytor.php

--- edytor.php
+++ edytor.php
@@ -14,12 +14,7 @@
 <fieldset>
 <legend>Lista wynikowa:</legend>
 <?php
-$servername = "localhost";
-$username = "atortuulxmovtg";
-$password = "85f9b2b3be752b8c59218a1dd28c19f026488750c4a77d18054a95627f66810b";
-$dbname = "d79rdvlg8o4982";
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
+require 'connect.php';
 // Check connection
 if ($conn->connect_error) 
 {
