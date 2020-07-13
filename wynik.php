a68d963f34bd8b40a3be2b8a62d6b3417a8b1586
CezGaj <68070366+CezGaj@users.noreply.github.com>
2020-07-13 20:00:29 +0200

    Update wynik.php

--- wynik.php
+++ wynik.php
@@ -88,15 +88,7 @@ if($zaznaczone==false) $liczba=0;
 $wynik=$wynik+$liczba;
 //echo "$wynik<br>";
 }
-$servername = "localhost";
-$username = "atortuulxmovtg";
-$password = "85f9b2b3be752b8c59218a1dd28c19f026488750c4a77d18054a95627f66810b";
-$dbname = "d79rdvlg8o4982";
-$conn = new mysqli($servername, $username, $password, $dbname);
-if ($conn->connect_error) 
-{
-    die("Connection failed: " . $conn->connect_error);
-}
+include 'connect.php';
 $sql="INSERT INTO wyniki (nr_indeksu,wynik) VALUES ('$nr_indeksu','$wynik')"; 
 
 if ($conn->query($sql) !== TRUE) 
