db7feb0c9f98613a71e1a025b15264bd4ce51810
CezGaj <68070366+CezGaj@users.noreply.github.com>
2020-07-15 20:54:33 +0200

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,6 +1,8 @@
 <?php
 $db = parse_url(getenv("DATABASE_URL"));
 
+try
+{
 $pdo = new PDO("pgsql:" . sprintf(
     "host=%s;port=%s;user=%s;password=%s;dbname=%s",
     $db["host"],
@@ -9,6 +11,11 @@ $pdo = new PDO("pgsql:" . sprintf(
     $db["pass"],
     ltrim($db["path"], "/")
 ));
+$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch(PDOException $e)
+{
+  echo 'Error: ' . $e->getMessage();
+}
 
-//$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 ?>
