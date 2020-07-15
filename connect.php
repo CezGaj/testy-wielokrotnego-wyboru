2ac350fe571eec4a915e2ff63b66d213bb2274fe
CezGaj <68070366+CezGaj@users.noreply.github.com>
2020-07-15 19:53:00 +0200

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,11 +1,12 @@
 <?php
-$servername = parse_url(getenv('DATABASE_URL'));
-$username = "atortuulxmovtg";
-$password = "85f9b2b3be752b8c59218a1dd28c19f026488750c4a77d18054a95627f66810b";
-$dbname = "d79rdvlg8o4982";
-$conn = new mysqli($servername, $username, $password, $dbname);
-if ($conn->connect_error) 
-{
-    die("Connection failed: " . $conn->connect_error);
-}
+$db = parse_url(getenv("DATABASE_URL"));
+
+$pdo = new PDO("pgsql:" . sprintf(
+    "host=%s;port=%s;user=%s;password=%s;dbname=%s",
+    $db["host"],
+    $db["port"],
+    $db["user"],
+    $db["pass"],
+    ltrim($db["path"], "/")
+));
 ?>
