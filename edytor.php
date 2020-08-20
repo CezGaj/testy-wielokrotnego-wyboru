d0fdc598c1243bcd8baa887a6e074b5268848af8
CezGaj <68070366+CezGaj@users.noreply.github.com>
2020-08-20 21:09:52 +0200

    Update edytor.php

--- edytor.php
+++ edytor.php
@@ -10,26 +10,22 @@
 <script src="js/bootstrap.min.js"></script>
 </head>
 <body>
+<script>
+var lista = [];    
+</script>
 <form action="edytor.php" method="post" autocomplete="off">
 <fieldset>
-<legend>Lista wynikowa:</legend>
 <?php
 require 'connect.php';
 if(isset($_POST["deleteall"]))
 {
-$stmt = $pdo->query('TRUNCATE TABLE wyniki');
+$stmt = $pdo->exec('TRUNCATE TABLE wyniki');
 $stmt->closeCursor();	
 }
 $stmt = $pdo->query('SELECT * FROM wyniki ORDER BY nr_indeksu asc');
 if ($stmt->execute() > 0) 
 {
     $li=0;
-    if(isset($_POST["sav"])) 
-    {
-        date_default_timezone_set('Europe/Warsaw');
-        $t=date("Y-m-d-H-i-s");
-        $plik=fopen("wyniki-$t.txt","w");
-    }
     echo '<table class="table"><tr><th>numer indeksu</th><th>wynik</th></table>';
 while($row = $stmt->fetch()) 
 {
@@ -45,15 +41,14 @@ while($row = $stmt->fetch())
             else $usuniete=true; 
         }
     }
-    if(isset($_POST["sav"])) fwrite($plik,$row["nr_indeksu"]." ".$row["wynik"]."\n");
-    if($usuniete==false) echo '<div><input type="checkbox" id="'.$li.'" name="'.$row["nr_indeksu"].'" value="'.$row["wynik"].'"/><label for="'.$li.'"><table class="table"><tr><td>'.$row["nr_indeksu"].'</td><td>'.$row["wynik"].'</td></tr></table></label></div>'; 
+    if($usuniete==false) 
+    {
+        echo '<div><input type="checkbox" id="'.$li.'" name="'.$row["nr_indeksu"].'" value="'.$row["wynik"].'"/><label for="'.$li.'"><table class="table"><tr><td>'.$row["nr_indeksu"].'</td><td>'.$row["wynik"].'</td></tr></table></label></div>';
+        echo '<script>lista.push([json_encode($row["nr_indeksu"]),json_encode($row["wynik"])]);</script>';
+    }
     $li++;
 }
-    if(isset($_POST["sav"]))
-    {
-        fclose($plik);
-        echo "<script>window.open('wyniki-$t.txt','_blank');</script>";
-    }
+
 }
 
 $stmt->closeCursor();	
@@ -64,10 +59,32 @@ $stmt->closeCursor();
     <div class="row justify-content-center">  
         <div class="col-md-4"><center><input type="submit" class="btn btn-primary mb-2" name="deleteall" value="Usuń wszystko"/></center></div>
         <div class="col-md-4"><center><input type="submit" class="btn btn-primary mb-2" name="delet" value="Usuń zaznaczone"/></center></div>
-        <div class="col-md-4"><center><input type="submit" class="btn btn-primary mb-2" name="sav" value="Zapisz do pliku"/></center></div>
+        <div class="col-md-4"><center><input type="button" class="btn btn-primary mb-2" name="sav" value="Zapisz do pliku" onclick="zapisz();"/></center></div>
     </div>
 </div>
 <br>
 </form>
+<script>
+function zapisz()
+{
+    var tekst = "";
+    for(var i=0;i<lista.length;i++)
+    {
+        tekst += lista[i][0] + " " + lista[i][1] + "\n";   
+    }
+    var a = document.createElement("a");
+    document.body.appendChild(a);
+    a.style = "display: none";
+    var json = JSON.stringify(tekst);
+    var blob = new Blob([tekst], {type: "text/plain;charset=utf-8"});
+    var url = window.URL.createObjectURL(blob);
+    a.href = url;
+    a.download = fileName;
+    a.click();
+    window.URL.revokeObjectURL(url);
+}
+
+
+</script>
 </body>
 </html>
